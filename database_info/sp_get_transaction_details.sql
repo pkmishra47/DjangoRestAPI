37547e257d080b030b674a76a5deae350172c8d2
Pradeep Mishra <pkmishra.pradeep@gmail.com>
2019-09-28 11:18:56 +0530

    adding database schema and stored being used.

---
+++ database_info/sp_get_transaction_details.sql
@@ -0,0 +1,31 @@
+DELIMITER $$
+
+USE `testdb`$$
+
+DROP PROCEDURE IF EXISTS `sp_get_transaction_details`$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `sp_get_transaction_details`(
+	p_contact_number VARCHAR(15),
+	p_offset INT,
+	p_total_count INT,
+	OUT p_Msg TEXT
+)
+BEGIN
+	IF EXISTS(SELECT 1 FROM tblusers WHERE contact_number = p_contact_number)
+	THEN
+		SELECT DATE_FORMAT(tt.tran_date,'%Y-%M-%d %H:%i:%S') 'Transaction Date',
+			amount 'Amount', 
+			CASE 	WHEN tran_type = 'A' THEN 'Credit'
+				WHEN tran_type = 'D' THEN 'Debit'
+			END 'Transaction Type'
+		FROM tblusers tu 
+		INNER JOIN tbltransactions tt ON tt.contact_number = tu.contact_number
+		WHERE tu.contact_number = p_contact_number
+		ORDER BY tran_date DESC
+		LIMIT p_offset,p_total_count;
+	ELSE
+		SET p_Msg = "User Details doesn't exist."; 
+	END IF;
+END$$
+
+DELIMITER ;
---
+++ database_info/sp_set_balance.sql
@@ -0,0 +1,47 @@
+DELIMITER $$
+
+USE `testdb`$$
+
+DROP PROCEDURE IF EXISTS `sp_set_balance`$$
+
+CREATE DEFINER=`root`@`%` PROCEDURE `sp_set_balance`(
+	p_contact_number VARCHAR(15),
+	p_amount DECIMAL(12,2),
+	p_TranType VARCHAR(1),
+	OUT p_Msg TEXT
+)
+BEGIN
+	DECLARE p_current_balance DECIMAL(12,2) DEFAULT 0;
+	IF EXISTS(SELECT 1 FROM tblusers WHERE contact_number = p_contact_number)
+	THEN
+		START TRANSACTION;
+			SELECT balance INTO p_current_balance FROM tblusers WHERE contact_number = p_contact_number FOR UPDATE;
+			
+			IF (p_TranType = 'A')
+			THEN
+				UPDATE tblusers SET balance = balance + p_amount WHERE contact_number = p_contact_number;
+				
+				INSERT INTO tbltransactions(tran_type,contact_number,amount,tran_date)
+				VALUES(p_TranType,p_contact_number,p_amount,CURRENT_TIMESTAMP());
+				SET p_Msg = "Transaction Successful.";
+				
+			ELSEIF p_TranType = 'D'
+			THEN
+				IF (p_current_balance - p_amount) < 0
+				THEN
+					SET p_Msg = "Your balance is low. Please enter other amount.";
+				ELSE
+					UPDATE tblusers SET balance = balance - p_amount WHERE contact_number = p_contact_number;
+					
+					INSERT INTO tbltransactions(tran_type,contact_number,amount,tran_date)
+					VALUES(p_TranType,p_contact_number,p_amount,CURRENT_TIMESTAMP());					
+					SET p_Msg = "Transaction Successful.";
+				END IF;
+			END IF;
+		COMMIT;
+	ELSE
+		SET p_Msg = "User Details doesn't exist."; 
+	END IF;
+END$$
+
+DELIMITER ;
